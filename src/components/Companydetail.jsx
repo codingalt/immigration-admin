a8cede810c21d3114109e40adf750c97f4e75c29
Hamid Raza <englishking786@gmail.com>
2024-01-29 16:28:02 +0500

    asd

--- src/components/Companydetail.jsx
+++ src/components/Companydetail.jsx
@@ -1,33 +1,39 @@
-import React, { useEffect, useState, useRef } from 'react';
-import SideNavbar from './SideNavbar';
-import TopNavbar from './TopNavbar';
-import "../style/tablelistid.css"
-import { Link , NavLink, useParams } from 'react-router-dom';
-import { useGetApplicationsByCompanyQuery } from '../services/api/companyApi';
-import moment from 'moment';
-import Loader from './Loader';
+import React, { useEffect, useState, useRef } from "react";
+import SideNavbar from "./SideNavbar";
+import TopNavbar from "./TopNavbar";
+import "../style/tablelistid.css";
+import { Link, NavLink, useParams } from "react-router-dom";
+import { useGetApplicationsByCompanyQuery } from "../services/api/companyApi";
+import moment from "moment";
+import Loader from "./Loader";
 
 const Companydetail = () => {
+  const { companyId, companyName } = useParams();
+  const { data, isLoading, isSuccess, error } =
+    useGetApplicationsByCompanyQuery(companyId);
+  console.log(data);
 
-      const { companyId, companyName } = useParams();
-      const { data,isLoading,isSuccess,error } = useGetApplicationsByCompanyQuery(companyId);
-      console.log(data);
+  return (
+    <div className="comapnayprofile-main-container">
+      <SideNavbar />
 
-    return (
-      <div className="comapnayprofile-main-container">
-        <div className="Addcompany-Topnavbar-client-profile-2">
-          <TopNavbar />
+      <div style={{ marginLeft: "11.8rem" }}>
+        <TopNavbar />
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <h2 className="changepassword-profile-heading">{companyName}</h2>
+
+          <Link to={`/servicelist/${companyId}`} style={{ marginTop: 10 }}>
+            <button className="Case-profile-addbtn" type="button">
+              Create New Case
+            </button>
+          </Link>
         </div>
 
-        <SideNavbar />
-
-        <h2 className="changepassword-profile-heading">{companyName}</h2>
-        <Link to={`/servicelist/${companyId}`}>
-          <button className="Case-profile-addbtn" type="button">
-            Create New Case
-          </button>
-        </Link>
-        <div className="table-list-sub-container table-list-cd" id='table-list2'>
+        <div
+          className="table-list-sub-container"
+          style={{ position: "relative" }}
+          id="table-list2"
+        >
           <table>
             <thead>
               <tr className="Table-heading">
@@ -101,7 +107,8 @@ const Companydetail = () => {
           </table>
         </div>
       </div>
-    );
-    }
+    </div>
+  );
+};
 
-export default Companydetail
+export default Companydetail;
